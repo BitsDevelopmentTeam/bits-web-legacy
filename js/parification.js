37af1b557a9078d489a1aae06e7e0edb85a263e4
Andrea Brancaleoni <virus@gmx.us>
2012-01-12 21:46:06 +0100

    chrome&safari fix

--- js/parification.js
+++ js/parification.js
@@ -7,7 +7,13 @@ function fakeWs() {
   var xhr = new XMLHttpRequest;
   xhr.open("GET",fakeUrl,true);
   xhr.onreadystatechange = function() {
-    self["onmessage"]({data: xhr.responseText});
+    if(self.onmessage != undefined) {
+      if(xhr.readyStatus === 4) {
+        self.onmessage({data: xhr.responseText});
+      }
+    } else {
+      setTimeout(function() {xhr.onreadystatechange()},100);
+    }
   }
   xhr.send();
 }
@@ -20,10 +26,31 @@ global.addEvent = function(obj,event,callback) {
   }
 }
 
-if(document.attachEvent !== undefined)
-  document.addEventListener = document.attachEvent;
+function isChrome() {
+  if(var match = navigator.userAgent.match(/Chrome\/(\w+)/)) {
+    this.version = new Number(match[1]);
+    return this;
+  }
+}
+
+function isSafari() {
+  return navigator.userAgent.match(/Safari/);
+}
+
+if( isSafari() !== false ) {
+  var chr = isChrome();
+  if(chr !== false) {
+    //is Chrome
+    if(chr.version <= 13 && >= 4) {
+      // && the version is between 13 and 4
+      global.WebSocket = fakeWs;
+    }
+  } else {
+    // is Safari
+    global.WebSocket = fakeWs;
+  }
+}
 
 global.WebSocket = global.WebSocket || global.MozWebSocket || fakeWs;
-global.fakeWs = fakeWs;
 
 })(this);
--- js/ws.js
+++ js/ws.js
@@ -1,9 +1,5 @@
 addEvent(window,"load",function() {
   var ws = new WebSocket("ws://bits.poul.org:3389");
-  // Handle Safari && old chromium madness
-  ws.onclose = function() { 
-    ws = new fakeWs();
-  }
   var handler = new Handler(browserHandler);
   ws.onmessage = function(event) { handler.webSocket(event); };
 });
