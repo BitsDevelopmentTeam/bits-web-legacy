b7f759e5cab29efed6ea5ea818cb734bafa74452
Andrea Brancaleoni <virus@gmx.us>
2012-01-24 19:00:47 +0100

    status.php JSON fix

--- status.php
+++ status.php
@@ -1,4 +1,4 @@
 <?php
-  $json = json_decode(http_get("http://bits.poul.org/data.json"));
-  echo ($json["value"] ? 1 : 0);
+  $json = json_decode(file_get_contents("http://bits.poul.org/data.json"));
+  echo ($json->status->value == "open" ? 1 : 0);
 ?>
