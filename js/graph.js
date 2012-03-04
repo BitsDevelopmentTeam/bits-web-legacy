f703d7b0805373ac01c07bce54fb57b48a3e925f
Andrea Brancaleoni <null@gmx.it>
2012-03-04 11:33:23 +0100

    graph initial commit

---
+++ js/graph.js
@@ -0,0 +1,12 @@
+/**
+ * @overview this is a module that exports a simplified interface to Bluff graph library 
+ *
+ * @author Andrea Brancaleoni
+ * @version 2012/03/04
+ * 
+ */
+
+module("graph", function(require, exports) {
+    "use strict";
+
+});
